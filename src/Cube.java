17e935ce129f03c8eb5feb3a658aed41ea739529
Romass <haker228708@gmail.com>
2023-10-02 20:37:37 +0600

    FIX: Fixed function getVolume

--- src/Cube.java
+++ src/Cube.java
@@ -3,10 +3,9 @@ public class Cube implements Shape{
     private double volume;
     public Cube(double width){
         this.width=width;
-        volume=width*width*width;
     }
     @Override
     public double getVolume() {
-        return volume;
+        return width*width*width;
     }
 }

4eba86a4dbc1722a5bc23e5a5a4dd54ba81a7d94
Romass <haker228708@gmail.com>
2023-10-04 18:36:29 +0600

    FEAT: Added function calls in main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello and welcome!");
+
+        System.out.println("Hello and welcome!");
+        Cube cube1 = Cube.builder().width(4).build();
+        Pyramid pyramid1 = new Pyramid(45, 40);
+        System.out.println(cube1.getVolume());
+
+        System.out.println(pyramid1.getBaseWidth());
+        System.out.println(pyramid1.getHeight());
+        System.out.println(pyramid1.getVolume());
     }
 }
